50594a7b8d434d111c7f9f2237de0becf0bf6d34
sadoozii <sadoozii@sadoozii>
2025-06-09 20:53:21 +0500

    Worked On Post method with postman

--- NodeJs/Files/HTTP.js
+++ NodeJs/Files/HTTP.js
@@ -1,21 +1,26 @@
-import express from 'express';
-import dotenv from dotenv;
-const app = express();
-const Port = process.env.PORT;
+import express from "express";
+import dotenv from "dotenv";
+
 dotenv.config();
 
+const app = express();
+const Port = process.env.PORT || 3000;
+
+//& Middleware
 app.use(express.json());
 
-
-app.get('/' , (req , res)=>{
-    res.send("Home Page");
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
 
-app.post('/user' , (req , res)=>{
-    const data = req.body;
-})
-  
-app.listen(Port, ()=>{
+app.post("/user", (req, res) => {
+  const { name, age, email } = req.body;
+  console.log(`Data Recieved: ${req.body}`);
+  res.json({ messgae: "Data Received" , name, age, email});
+});
 
+
+
+app.listen(Port, () => {
   console.log(`🚀 Server running on http://localhost:${Port}`);
-})
+});
